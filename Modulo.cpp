acf9e31b29b60893a047d20602a82eb1e94ced27
rupesh <cgcrupesh@gmail.com>
2023-12-03 09:40:30 +0530

    Modulo

--- Modulo.cpp
+++ Modulo.cpp
@@ -3,6 +3,10 @@ Given a Number N, Print its factorial.
 Constraints
 1 <= N <= 100 
 
+Print answer modulo M
+where M = 47
+
+(A % M < 47)
 */
 
 #include<bits/stdc++.h>
@@ -12,8 +16,11 @@ int main(){
     int n;
     cin>>n;
     long long fact = 1;
+
+    int M = 47;
+
     for (int i=2 ; i<=n ; i++){
-        fact = fact * i;
+        fact = (fact * i) % M;
     }
 
     cout<<"Factorial: "<<fact;
