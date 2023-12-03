ede8e51ed104f7a5cf313305cba5fcc0d369a79e
rupesh <cgcrupesh@gmail.com>
2023-12-03 09:34:15 +0530

    Factorial

--- Modulo.cpp
+++ Modulo.cpp
@@ -1,8 +1,22 @@
+/* 
+Given a Number N, Print its factorial.
+Constraints
+1 <= N <= 100 
+
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-    
+    int n;
+    cin>>n;
+    long long fact = 1;
+    for (int i=2 ; i<=n ; i++){
+        fact = fact * i;
+    }
+
+    cout<<"Factorial: "<<fact;
 
     return 0;
 }
